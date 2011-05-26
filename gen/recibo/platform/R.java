5394d0b5eb7f7ca8da96aeba913848e1be5e32d0
Lere Williams <lere.williams@gmail.com>
2011-05-26 00:46:12 -0700

    added R.java

--- gen/recibo/platform/R.java
+++ gen/recibo/platform/R.java
@@ -14,18 +14,16 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050000;
-        public static final int tableRow1=0x7f050001;
-        public static final int textView1=0x7f050002;
-        public static final int textView2=0x7f050003;
-        public static final int textview=0x7f050004;
+        public static final int tableRow1=0x7f050000;
+        public static final int textView1=0x7f050001;
+        public static final int textView2=0x7f050002;
+        public static final int textview=0x7f050003;
     }
     public static final class layout {
-        public static final int add_user=0x7f030000;
-        public static final int billshare_receiptview=0x7f030001;
-        public static final int list_item=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int main_billshare=0x7f030004;
+        public static final int billshare_receiptview=0x7f030000;
+        public static final int list_item=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int main_billshare=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
